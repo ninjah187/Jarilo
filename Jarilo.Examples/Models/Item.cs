4adba08062b4d38d01040672b9422e60ccc1a908
Karol Hinz <karolhnz@gmail.com>
2017-11-07 19:16:28 +0100

    example enum model

---
+++ Jarilo.Examples/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jarilo.Examples.Models
+{
+    enum Item
+    {
+        None,
+
+        [Value("sleeping-dart", "Sleeping dart.")]
+        SleepingDart,
+
+        [Value("poison-dart", "Poison dart.")]
+        PoisonDart,
+
+        [Value("healing-potion", "Healing potion.")]
+        HealingPotion,
+
+        [Value("stamina-potion", "Stamina potion.")]
+        StaminaPotion
+    }
+}
---
+++ Jarilo.Examples/Models/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jarilo.Examples.Models
+{
+    enum Weapon
+    {
+        None,
+
+        [Value("katana", "Melee weapon. Powerful but slow.")]
+        Katana,
+
+        [Value("shuriken", "Ranged weapon. Good for harassing enemies.")]
+        Shuriken,
+
+        [Value("tanto", "Melee weapon. Fast but weak.")]
+        Tanto
+    }
+}
