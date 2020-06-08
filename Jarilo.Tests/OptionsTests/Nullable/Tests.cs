0a669f2655e41688e39c1d74bcf832bdcd45030c
dwright78 <dwright78@gatech.edu>
2020-06-08 18:08:38 -0500

    Support for Options of a nullable type

---
+++ Jarilo.Tests/OptionsTests/Nullable/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jarilo.Tests.OptionsTests.Nullable
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task NoOption_DefaultValue()
+        {
+            var args = Command.Name;
+            await AppTest.Run(args, output =>
+            {
+                output.AssertOption(null);
+            });
+        }
+
+        [Fact]
+        public async Task DotSeparator_Success()
+        {
+            var args = $"{Command.Name} --nullable 47";
+            await AppTest.Run(args, output =>
+            {
+                output.AssertOption(47);
+            });
+        }
+
+        [Fact]
+        public async Task CommaSeparator_Success()
+        {
+            var args = $"{Command.Name} --nullable 47";
+            await AppTest.Run(args, output =>
+            {
+                output.AssertOption(47);
+            });
+        }
+
+        [Fact]
+        public async Task Many_FirstInteger_OnlyFirstRead()
+        {
+            var args = $"{Command.Name} --nullable 47 any values 1 2 3";
+            await AppTest.Run(args, output =>
+            {
+                output.AssertOption(47);
+            });
+        }
+
+        [Fact]
+        public async Task Many_FirstNotInteger_ThrowsException()
+        {
+            var args = $"{Command.Name} --nullable not-integer 47 any values 1 2 3";
+            await AppTest.Run(args, output =>
+            {
+                output.AssertParsingException("--nullable", "not-integer");
+            });
+        }
+    }
+}
