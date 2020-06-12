2871b4306f1efa4363d22253f08bd499d562ce05
dwright78 <dwright78@gatech.edu>
2020-06-11 20:59:53 -0500

    Minor, fix whitespace issue

--- Jarilo.Tests/HelpTests/EnumValues.cs
+++ Jarilo.Tests/HelpTests/EnumValues.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Jarilo.Tests.HelpTests.Enum
 {
     static class EnumInfo
-	{
+    {
         internal const string Value1txt = "value-1";
         internal const string Value1Description = "Enum value 1.";
         internal const string Value2txt = "value-2";
@@ -21,8 +21,8 @@ namespace Jarilo.Tests.HelpTests.Enum
 
         [Value(EnumInfo.Value2txt, "")]
         Value2,
-		
-		[Value(EnumInfo.Value3txt, null)]
-		Value3
+
+        [Value(EnumInfo.Value3txt, null)]
+        Value3
     }
 }
