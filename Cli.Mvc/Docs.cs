38381b04db60b2ed67cbb84f137d0d1f9cecde47
Karol Hinz <karol@hinz.tech>
2022-12-04 12:08:07 +0100

    Add docs drafts

---
+++ Cli.Mvc/Docs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Cli.Mvc
+{
+    public abstract class Docs
+    {
+        public void Describe<T>(string description)
+        {
+        }
+
+        public void Describe<T>(string description, Expression<Action<T>> descriptor)
+        {
+        }
+
+        public T Description<T>(params Expression<Func<string>>[] description)
+        {
+            return default;
+        }
+    }
+}
---
+++ Cli.Mvc/Documentator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cli.Mvc
+{
+    public class Documentation
+    {
+        readonly IRouter _router;
+
+        public Documentation(IRouter router)
+        {
+            _router = router;
+        }
+
+        //public string GetDescription(string path)
+        //{
+        //    foreach (var route in _router.Routes)
+        //    {
+        //        if (route.Path == path)
+        //        {
+        //            // action
+        //        }
+        //        if (route.ControllerPath == path)
+        //        {
+        //            // controller
+        //        }
+        //    }
+        //}
+
+        //string GetActionDescription(string path, Route route)
+        //{
+            
+        //}
+    }
+}
